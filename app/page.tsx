4f81b3c6dd8a6a8214529531e938be0630dfc4cb
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-10-13 23:23:18 -0600

    Add keys and make files

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
-import Image from "next/image";
+'use client';
+
+
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+//Agregada para saber que la clave publica esté definida
+if(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined){
+  throw new Error("NEXT_PUBLIC_STRIPE_PUBLIC_KEY is not defined()");
+}
+
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
 export default function Home() {
+  //Variable definida para colocar el MONTO a pagar
+  const amount =4.99;
+
   return (
     <div className="">
         <h1>MAIN</h1>
