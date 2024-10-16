dd0f6014881c2aff00b209209466b3c73d6a9897
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-10-15 23:41:57 -0600

    Add the function for make a payment intent

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -1,10 +1,20 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 //secret key
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(request : NextRequest) {
     try {
         const {amount} = await request.json();
+
+        //CREATE A PAYMENT INTENT
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount,
+            currency: "usd",
+            automatic_payment_methods: {enabled:true}, //De a cuerdo al navegador da los pagos disponibles
+        });
+
+        //Da el client secret
+        return NextResponse.json({clientSecret: paymentIntent.client_secret});
     } catch (error) {
         
     }
