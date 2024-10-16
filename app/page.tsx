e01e140d553528f1cab9525de7ba465d00a313cb
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-10-15 22:55:02 -0600

    Add word to principal page

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,15 @@ export default function Home() {
   const amount =4.99;
 
   return (
-    <div className="">
-        <h1>MAIN</h1>
-    </div>
+    <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr
+     from-blue-500 to-purple-500">
+      <div className="mb-10">
+        <h1 className="text-4xl font-extrabold mb-2">Sonny</h1>
+        <h2 className="text-2xl">
+          has requested
+          <span className="font-bold">${amount}</span>
+        </h2>
+      </div>
+    </main>
   );
 }
