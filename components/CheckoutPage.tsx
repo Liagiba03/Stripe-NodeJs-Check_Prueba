733367665dcf7fecce6b819500e198d8c65ac9b3
Liagiba03 <143125742+Liagiba03@users.noreply.github.com>
2024-10-16 21:01:00 -0600

    Add the pay, styles and button

--- components/CheckoutPage.tsx
+++ components/CheckoutPage.tsx
@@ -30,12 +30,38 @@ const CheckoutPage = ({ amount } : {amount: number}) => {
         .then((data) => setClientSecret(data.clientSecret));
     }, [amount]);
 
+    //Function for the form
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) =>{
+        event.preventDefault();
+        setLoading(true);
+
+        if(!stripe || !elements){
+            return;
+        }
+
+        //verificar si existen errores
+        const { error : submitError} = await elements.submit();
+
+        if (submitError){
+            setErrorMessage(submitError.message);
+            setLoading(false);
+            return;
+        }
+    }
+    
     return(
-        <form>
+        <form onSubmit={handleSubmit} className="bg-white p-2 rounded-md">
             {clientSecret && <PaymentElement/>}
-            <button>PAY</button>
+            {errorMessage && <div>{errorMessage}</div>}
+            <button
+                disabled={!stripe || loading}
+                className="text-white w-full p-5 bg-black mt-2 rounded-md font-bold disabled:opacity-50 disabled:animate-pulse"
+                >
+                {!loading ? `Pay $${amount}`:"Processing ..."}
+            </button>
         </form>
-    )
+    );
 };
+
 
 export default CheckoutPage;
